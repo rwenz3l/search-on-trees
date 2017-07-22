376b52d407d14bff85eb679aef30dfa7f33b7cfa
doge <info@robin-wenzel.de>
2017-07-22 21:49:16 +0200

    cleaning..

--- recv.php
+++ recv.php
@@ -1,26 +1,25 @@
 <?php
-/* Debug
-print_r($_POST);
-print_r($_FILES);
-*/
+/* Check for $_FILES => logfiile and $_POST => sub content */
 if( isset($_FILES['logfile']) && isset($_POST['sub']) )
 {
+  /* Init Upload-Dir, Create if not exists */
   $uploaddir = "index/" . $_POST['sub'] . "/";
   if ( ! file_exists($uploaddir) ) {
     mkdir($uploaddir, 0777, true);
   }
+
+  /* Init Upload-File, delete if version exists */
   $uploadfile = $uploaddir . basename( $_FILES['logfile']['name']);
   if ( file_exists($uploadfile) ) {
     echo("Delete old file..");
     unlink($uploadfile);
   }
-  if(move_uploaded_file($_FILES['logfile']['tmp_name'], $uploadfile))
-  {
-    echo "The file has been added to the index";
-  }
-  else
-  {
-    echo "There was an error!";
+
+  /* Save Recieved File */
+  if(move_uploaded_file($_FILES['logfile']['tmp_name'], $uploadfile)) {
+    echo("The file has been added to the index" . PHP_EOL);
+  } else {
+    echo("There was an error!");
     print_r($_FILES);
   }
 }
