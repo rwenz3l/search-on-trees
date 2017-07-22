f38bc2472c50f94ab4fc93065d74a2017787a711
doge <info@robin-wenzel.de>
2017-07-22 21:53:59 +0200

    raise result limit

--- grep.php
+++ grep.php
@@ -8,7 +8,7 @@
             $case_sensitive = true;
         }
         $search_path = $_POST['search_path'];
-        $command = "grep -r" . ($case_sensitive ? "" : "i") . "m 100" . " " . escapeshellarg($_POST['grep_pattern']) . " " . $search_path . "";
+        $command = "grep -r" . ($case_sensitive ? "" : "i") . "m 5000" . " " . escapeshellarg($_POST['grep_pattern']) . " " . $search_path . "";
         $result = -1;
         $return_code = -1;
         $result = exec($command, $output, $return_code);
@@ -26,7 +26,7 @@
 <!-- Output Start -->
 <?php if (!empty($command)) { ?>
     <code>Command: <?php echo htmlspecialchars($command); ?></code><br>
-    <code>Results Limited to: <?php echo count($output); ?></code><br>
+    <code>Results Limited to: 5000, Found: <?php echo count($output); ?></code><br>
 	----------------------------------------------------------------<br>
     <?php echo $formatted; ?>
     
